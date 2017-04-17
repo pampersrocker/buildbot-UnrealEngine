499d26b1ddc537571c9dba0222df7215ac969bc8
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2017-04-17 18:30:04 +0200

    Added sub packages to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-long_description ="""
+long_description = """
 buildbot-UnrealEngine
 =====================
 
@@ -12,36 +12,41 @@ Documentation
 See https://github.com/pampersrocker/buildbot-UnrealEngine for documentation.
 """
 
-VERSION="0.1.0"
+VERSION = "0.1.0"
 
 setup(
-  name="buildbot_UnrealEngine",
-  version=VERSION,
-  description="Easy configuration for the Unreal Automation Tool",
-  long_description=long_description,
-  author="Marvin Pohl",
-  author_email="mp120@hdm-stuttgart.de",
-  url="https://github.com/pampersrocker/buildbot-UnrealEngine",
-  packages=["buildbot_UnrealEngine"],
-  requires=["Buildbot (>=0.9.1)"],
-  entry_points= {
-    'buildbot.steps': [
-      'BuildCookRun = buildbot_UnrealEngine.AutomationTool:BuildCookRun',
-      'UEBuild = buildbot_UnrealEngine.BuildTool:Build',
-      'UERebuild = buildbot_UnrealEngine.BuildTool:Rebuild',
-      'UEClean = buildbot_UnrealEngine.BuildTool:Clean',
+    name="buildbot_UnrealEngine",
+    version=VERSION,
+    description="Easy configuration for the Unreal Automation Tool",
+    long_description=long_description,
+    author="Marvin Pohl",
+    author_email="mp120@hdm-stuttgart.de",
+    url="https://github.com/pampersrocker/buildbot-UnrealEngine",
+    packages=[
+        "buildbot_UnrealEngine",
+        "buildbot_UnrealEngine.BuildTool",
+        "buildbot_UnrealEngine.AutomationTool",
+        "buildbot_UnrealEngine.UnrealCommand"
+    ],
+    requires=["Buildbot (>=0.9.1)"],
+    entry_points={
+        'buildbot.steps': [
+            'BuildCookRun = buildbot_UnrealEngine.AutomationTool:BuildCookRun',
+            'UEBuild = buildbot_UnrealEngine.BuildTool:Build',
+            'UERebuild = buildbot_UnrealEngine.BuildTool:Rebuild',
+            'UEClean = buildbot_UnrealEngine.BuildTool:Clean',
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: C++",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Build Tools",
     ]
-  },
-  classifiers=[
-    "Development Status :: 3 - Alpha",
-    "Environment :: Plugins",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: Microsoft :: Windows",
-    "Operating System :: MacOS",
-    "Operating System :: POSIX :: Linux",
-    "Programming Language :: C++",
-    "Programming Language :: Python :: 2.7",
-    "Topic :: Software Development :: Build Tools",
-  ]
 )
